53789247ef835c971159aee984585322aee4de70
JoseGHdezC <alu0101545820@ull.edu.es>
2024-09-24 12:17:11 +0100

    Git Scripts

---
+++ git_basic_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Github login script"
+
+echo "git config --global user.email alu0101545820@ull.edu.es"$(git config --global user.email alu0101545820@ull.edu.es)
+echo "git config --global user.name "JoseGHdezC""$(git config --global user.name "JoseGHdezC")
