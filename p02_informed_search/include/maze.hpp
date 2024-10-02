91ccfeddf0aa4e9fca34637389a3f57052231b3a
JoseGHdezC <alu0101545820@ull.edu.es>
2024-10-02 13:50:44 +0100

    P02

---
+++ p02_informed_search/include/maze.hpp
@@ -0,0 +1,14 @@
+#ifndef MAZE_HPP
+#define MAZE_HPP
+
+#include <vector>
+#include <iostream>
+
+class Maze {
+ public:
+
+ private:
+  std::vector<std::vector<unsigned>> maze_;
+  unsigned row_size_{0};
+};
+#endif
